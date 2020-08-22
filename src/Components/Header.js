3494fa9b493e828adea30e66d22efe2c726fc78e
Garland Lai <garlandlai24@gmail.com>
2020-08-21 18:39:15 -0700

    attempt to add animation. Still needs work

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import '../header.css';
 
-const HeaderContainer = styled.div``;
+// const HeaderContainer = styled.div``;
 
-const StyledHeader = styled.div`
-	color: black;
-	font-size: 70px;
-	font-weight: 700;
-	padding: 50px 0;
-	height: 200px;
-	width: 100vw;
-	background-color: grey;
-`;
+// const StyledHeader = styled.div`
+// 	color: black;
+// 	font-size: 70px;
+// 	font-weight: 700;
+// 	padding: 50px 0;
+// 	height: 200px;
+// 	width: 100vw;
+// 	background-color: grey;
+// `;
 
 const Header = () => {
 	return (
-		<HeaderContainer>
-			<StyledHeader>Random Tools</StyledHeader>
-		</HeaderContainer>
+		<div class="containerWrapper">
+			<div class="container">
+				<h1>
+					<span class="first-word">Tool</span>
+					<span class="last-word">Belt</span>
+				</h1>
+			</div>
+		</div>
 	);
 };
 
