b89a762acfa70d71a535709502779ac1a9676380
Garland Lai <garlandlai24@gmail.com>
2020-08-24 09:53:55 -0700

    clean up header files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
 import '../header.css';
-
-// const HeaderContainer = styled.div``;
-
-// const StyledHeader = styled.div`
-// 	color: black;
-// 	font-size: 70px;
-// 	font-weight: 700;
-// 	padding: 50px 0;
-// 	height: 200px;
-// 	width: 100vw;
-// 	background-color: grey;
-// `;
 
 const Header = () => {
 	return (
